9a0c31dffecc1bf898ceecff9ad4fec7902e3392
imane_qandyl <102863547+imane-qandyl@users.noreply.github.com>
2024-09-30 11:14:16 +0400

    Update union.c

--- level2/union.c
+++ level2/union.c
@@ -27,31 +27,32 @@
 // $>
 #include <unistd.h>
 
-void ft_union(char *s1,char *s2)
+void    ft_union(char *s1, char *s2)
 {
-  int array[256] = {0};
-  while(*s1)
-  {
-    if(!array[(unsigned char)*s1])//not seen
+    unsigned char seen[256] = {0};
+    while(*s1)
     {
-        write(1,s1,1);
-        array[(unsigned char)*s1] = 1;
+        if(!seen[(unsigned char)*s1])
+        {   
+             write(1,s1,1);
+            seen[(unsigned char)*s1] = 1;
+        }
+        s1++;
     }
-    s1++;
-  }
-   while(*s2)
-  {
-    if(!array[(unsigned char)*s2])
+    while(*s2)
     {
-         write(1,s2,1);
-        array[(unsigned char)*s2] = 1;
+        if(!seen[(unsigned char)*s2])
+        {    
+            write(1,s2,1);
+            seen[(unsigned char)*s2] = 1;
+        }
+        s2++;
     }
-    s2++;
-  }
 }
-int main(int argc,char **argv)
+int main(int argc, char **argv)
 {
     if(argc == 3)
         ft_union(argv[1],argv[2]);
     write(1,"\n",1);
+    return(0);
 }
