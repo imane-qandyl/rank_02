e68fd7e4342011b6ebb717333075a6d880861882
imane_qandyl <102863547+imane-qandyl@users.noreply.github.com>
2024-10-02 10:23:27 +0400

    Update ft_strcpy.c

--- level1/ft_strcpy.c
+++ level1/ft_strcpy.c
@@ -8,15 +8,17 @@
 // Your function must be declared as follows:
 
 // char    *ft_strcpy(char *s1, char *s2);
-char    *ft_strcpy(char *s1, char *s2)
+char	*ft_strcpy(char *s1, char *s2)
 {
-    int i = 0;
-    while(s1[i])
-    {
-        s1[i] = s2[i];
-        i++;
-    }
-    return(s1);
+	int i = 0;
+	
+	while (s2[i] != '\0')
+	{
+		s1[i] = s2[i];
+		i++;
+	}
+	s1[i] = '\0';
+	return (s1);
 }
 // #include <stdio.h>
 // int main()
